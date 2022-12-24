8f8d4dfad5bb19bb0f85673d72edef35cfe18388
NinaZhang04 <healtoneforever@gmail.com>
2022-12-24 16:32:11 -0500

    script todo

--- todolistscript.js
+++ todolistscript.js
@@ -1,1 +1,19 @@
+const listContainer = document.querySelector('[data-lists]')
+
 let lists = []
+
+function render() {
+    clearElement(listContainer)
+    lists.forEach(list => {
+        const listElement = document.createElement('li')
+        listElement.classList.add("list-name")
+        listElement.innerText = list
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+
+}
+
+render()
