68436162ec23cb87b6c42d14033ca1d4aa96cca1
NinaZhang04 <healtoneforever@gmail.com>
2022-12-24 21:25:32 -0500

    finally fixed a bug. deleted const e = something If anything goes horribly wrong return to this stage

--- todolistscript.js
+++ todolistscript.js
@@ -1,11 +1,35 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
-const newListInput = document.querySelector('[data-new-list-input')
+const newListInput = document.querySelector('[data-new-list-input]')
+const LOCAL_STOARGE_LIST_KEY = 'task.lists'
+const LOCAL_STOARGE_SELECTED_LIST_ID_KEY = 'task.selectedListID'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STOARGE_LIST_KEY)) || []
+let selectedListID = localStorage.getItem(LOCAL_STOARGE_SELECTED_LIST_ID_KEY)
 
-let lists = [
-    {id: 1, name: 'name'},
-    {id: 2, name: 'todo'}
-]
+listsContainer.addEventListener('click', e=> {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedListID = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+
+//save the current list into the local storage
+function save(){
+    localStorage.setItem(LOCAL_STOARGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem
+    (LOCAL_STOARGE_SELECTED_LIST_ID_KEY, selectedListID)
+}
+
+
+function saveAndRender(){
+    save()
+    render()
+}
+
+//add this fucntion later that allow users to save their information to the cloud
+function saveToCloud() {
+
+}
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -14,8 +38,10 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    render()
+    saveAndRender()
 })
+
+
 
 
 function render() {
@@ -24,7 +50,10 @@ function render() {
         const listElement = document.createElement('li')
         listElement.dataset.listId = list.id
         listElement.classList.add("list-name")
-        listElement.innerText = list
+        listElement.innerText = list.name
+        if(list.id === selectedListID) {
+            listElement.classList.add('active-list')
+        } 
         listsContainer.appendChild(listElement)
     })
 }
